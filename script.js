1307ac8457e2f96ead164a167134ef9d2b5a41e6
Rasyiid Putra Wijaya <146507065+ofsyid@users.noreply.github.com>
2026-04-23 09:54:45 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,63 @@
+function login() {
+    const user = document.getElementById("username").value;
+    const pass = document.getElementById("password").value;
+    const alertBox = document.getElementById("alert");
+
+
+    if (user === "rasyid" && pass === "1") {
+
+        alertBox.style.display = "block";
+        alertBox.className = "alert success";
+        alertBox.textContent = "Login berhasil!";
+
+
+        setTimeout(() => {
+            window.location.href = "../index.html";
+        }, 300);
+    } else if (user === "rasyid" && pass === "12") {
+        alertBox.style.display = "block";
+        alertBox.className = "alert success";
+        alertBox.textContent = "Login berhasil!";
+
+      
+        setTimeout(() => {
+            window.location.href = "../cv.html";
+        }, 300);
+    }  else if (user === "rasyid" && pass === "123") {
+        alertBox.style.display = "block";
+        alertBox.className = "alert success";
+        alertBox.textContent = "Login berhasil!"; 
+
+           setTimeout(() => {
+            window.location.href = "https://ofsyid.github.io/fortofolio/index.html";
+        }, 300);
+    }  else if (user === "rasyid" && pass === "1234") {
+        alertBox.style.display = "block";
+        alertBox.className = "alert success";
+        alertBox.textContent = "Login berhasil!";
+
+      
+        setTimeout(() => {
+            window.location.href = "https://www.blogger.com/profile/14165569268417189496";
+        }, 300);
+    }
+    else {
+        alertBox.style.display = "block";
+        alertBox.className = "alert error";
+        alertBox.textContent = "Masukkin yang bener!";
+    }
+}
+function toggleDarkMode() {
+    const body = document.body;
+    const btn = document.querySelector('.toggle-btn');
+    body.classList.toggle('dark');
+
+
+    if (body.classList.contains('dark')) {
+        btn.textContent = "Light Mode";
+    } else {
+        btn.textContent = "Dark Mode";
+    }
+
+}
+
